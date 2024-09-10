eda510290ff29e492c228b761146b850da0d2cb0
CodeKiro <kirofiniello@outlook.it>
2024-09-10 17:32:15 +0200

    I added the route in the web file to access the show of the logged in user under the "auth" middleware to access its property Auth::user()->id , i.e. show in this case the show of the logged in user, and then I did nothing but modify this thing in the app.blade, which contains the navbar

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\OrderController;
@@ -6,53 +7,24 @@ use App\Http\Controllers\Admin\HomeController as AdminHomeController;
 use App\Http\Controllers\Admin\DishController;
 use App\Http\Controllers\Admin\TypologyController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
 
 Auth::routes();
 
-// Route::get('/home', [GuestHomeController::class, 'index'])->name('home');
-
-Route::get('/restourant', [AdminHomeController::class,'index'])->name('pages.index');
-Route::get('/show/{user}', [AdminHomeController::class,'show'])->name('pages.show');
+Route::get('/restourant', [AdminHomeController::class, 'index'])->name('pages.index');
+Route::get('/show/{user}', [AdminHomeController::class, 'show'])->name('pages.show');
 Route::post('/restourant', [AdminHomeController::class, 'store'])->name('pages.store');
+Route::get('/create', [AdminHomeController::class, 'create'])->name('pages.create');
 
-Route::get('/create', [AdminHomeController::class,'create'])->name('pages.create');
-
-Route::middleware('auth')->name('admin.')->prefix('admin/')->group(function(){
-        //Rotte protette
-    // Route::get('secret-home', [AdminHomeController::class, 'index'])->name('home');
-
+Route::middleware('auth')->name('admin.')->prefix('admin')->group(function() {
     Route::resource('orders', OrderController::class);
-    Route::resource('show', OrderController::class);
-
     Route::resource('dishes', DishController::class);
-    Route::resource('/show', DishController::class);
-    Route::resource('/create', DishController::class);
     Route::resource('typologies', TypologyController::class);
 
-    // Route::resource('/show',AdminHomeController::class);
-
-    }
-);
-
-
-
-Auth::routes();
+    // Route for viewing the authenticated user's restaurant
+    Route::get('user/{user}', [AdminHomeController::class, 'show'])->name('user.show');
+});
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-
-
