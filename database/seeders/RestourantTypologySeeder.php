2ba3e0d3b9565a101b60402cb841bfda53cf5909
afablelorenz <afablelorenz@gmail.com>
2024-09-18 22:53:43 +0200

    test filter

--- database/seeders/RestourantTypologySeeder.php
+++ database/seeders/RestourantTypologySeeder.php
@@ -14,22 +14,40 @@ public function run(): void{
     $typologies = Typology::all();
     $now = now();
 
-    $users[0]->typologies()->attach($typologies[7]->id, ['created_at' => $now, 'updated_at' => $now]);
-    $users[1]->typologies()->attach($typologies[1]->id, ['created_at' => $now, 'updated_at' => $now]);
-    $users[2]->typologies()->attach($typologies[4]->id, ['created_at' => $now, 'updated_at' => $now]);
-    $users[3]->typologies()->attach($typologies[3]->id, ['created_at' => $now, 'updated_at' => $now]);
-    $users[4]->typologies()->attach($typologies[5]->id, ['created_at' => $now, 'updated_at' => $now]);
-    $users[5]->typologies()->attach($typologies[6]->id, ['created_at' => $now, 'updated_at' => $now]);
-    $users[6]->typologies()->attach($typologies[5]->id, ['created_at' => $now, 'updated_at' => $now]);
-    $users[7]->typologies()->attach($typologies[2]->id, ['created_at' => $now, 'updated_at' => $now]);
-    $users[8]->typologies()->attach($typologies[8]->id, ['created_at' => $now, 'updated_at' => $now]);
-    $users[9]->typologies()->attach($typologies[9]->id, ['created_at' => $now, 'updated_at' => $now]);
-    $users[10]->typologies()->attach($typologies[1]->id, ['created_at' => $now, 'updated_at' => $now]);
-    $users[11]->typologies()->attach($typologies[2]->id, ['created_at' => $now, 'updated_at' => $now]);
-    $users[12]->typologies()->attach($typologies[3]->id, ['created_at' => $now, 'updated_at' => $now]);
-    $users[13]->typologies()->attach($typologies[4]->id, ['created_at' => $now, 'updated_at' => $now]);
-    $users[11]->typologies()->attach($typologies[5]->id, ['created_at' => $now, 'updated_at' => $now]);
-    $users[12]->typologies()->attach($typologies[6]->id, ['created_at' => $now, 'updated_at' => $now]);
+    $users[0]->typologies()->syncWithoutDetaching($typologies[7]->id, ['created_at' => $now, 'updated_at' => $now]);
+    $users[1]->typologies()->syncWithoutDetaching($typologies[1]->id, ['created_at' => $now, 'updated_at' => $now]);
+    $users[2]->typologies()->syncWithoutDetaching($typologies[4]->id, ['created_at' => $now, 'updated_at' => $now]);
+    $users[3]->typologies()->syncWithoutDetaching($typologies[3]->id, ['created_at' => $now, 'updated_at' => $now]);
+    $users[4]->typologies()->syncWithoutDetaching($typologies[5]->id, ['created_at' => $now, 'updated_at' => $now]);
+    $users[5]->typologies()->syncWithoutDetaching($typologies[6]->id, ['created_at' => $now, 'updated_at' => $now]);
+    $users[6]->typologies()->syncWithoutDetaching($typologies[5]->id, ['created_at' => $now, 'updated_at' => $now]);
+    $users[7]->typologies()->syncWithoutDetaching($typologies[2]->id, ['created_at' => $now, 'updated_at' => $now]);
+    $users[8]->typologies()->syncWithoutDetaching($typologies[8]->id, ['created_at' => $now, 'updated_at' => $now]);
+    $users[9]->typologies()->syncWithoutDetaching($typologies[9]->id, ['created_at' => $now, 'updated_at' => $now]);
+    $users[10]->typologies()->syncWithoutDetaching($typologies[1]->id, ['created_at' => $now, 'updated_at' => $now]);
+    $users[11]->typologies()->syncWithoutDetaching($typologies[2]->id, ['created_at' => $now, 'updated_at' => $now]);
+    $users[12]->typologies()->syncWithoutDetaching($typologies[3]->id, ['created_at' => $now, 'updated_at' => $now]);
+    $users[13]->typologies()->syncWithoutDetaching($typologies[4]->id, ['created_at' => $now, 'updated_at' => $now]);
+    $users[11]->typologies()->syncWithoutDetaching($typologies[5]->id, ['created_at' => $now, 'updated_at' => $now]);
+    $users[12]->typologies()->syncWithoutDetaching($typologies[6]->id, ['created_at' => $now, 'updated_at' => $now]);
+
+    $users[0]->typologies()->syncWithoutDetaching($typologies[4]->id, ['created_at' => $now, 'updated_at' => $now]);
+    $users[1]->typologies()->syncWithoutDetaching($typologies[2]->id, ['created_at' => $now, 'updated_at' => $now]);
+    $users[2]->typologies()->syncWithoutDetaching($typologies[1]->id, ['created_at' => $now, 'updated_at' => $now]);
+    $users[3]->typologies()->syncWithoutDetaching($typologies[1]->id, ['created_at' => $now, 'updated_at' => $now]);
+    $users[4]->typologies()->syncWithoutDetaching($typologies[6]->id, ['created_at' => $now, 'updated_at' => $now]);
+    $users[5]->typologies()->syncWithoutDetaching($typologies[5]->id, ['created_at' => $now, 'updated_at' => $now]);
+    $users[6]->typologies()->syncWithoutDetaching($typologies[4]->id, ['created_at' => $now, 'updated_at' => $now]);
+    $users[7]->typologies()->syncWithoutDetaching($typologies[3]->id, ['created_at' => $now, 'updated_at' => $now]);
+    $users[8]->typologies()->syncWithoutDetaching($typologies[6]->id, ['created_at' => $now, 'updated_at' => $now]);
+    $users[9]->typologies()->syncWithoutDetaching($typologies[3]->id, ['created_at' => $now, 'updated_at' => $now]);
+    $users[10]->typologies()->syncWithoutDetaching($typologies[2]->id, ['created_at' => $now, 'updated_at' => $now]);
+    $users[11]->typologies()->syncWithoutDetaching($typologies[4]->id, ['created_at' => $now, 'updated_at' => $now]);
+    $users[12]->typologies()->syncWithoutDetaching($typologies[5]->id, ['created_at' => $now, 'updated_at' => $now]);
+    $users[13]->typologies()->syncWithoutDetaching($typologies[1]->id, ['created_at' => $now, 'updated_at' => $now]);
+    $users[11]->typologies()->syncWithoutDetaching($typologies[2]->id, ['created_at' => $now, 'updated_at' => $now]);
+    $users[12]->typologies()->syncWithoutDetaching($typologies[1]->id, ['created_at' => $now, 'updated_at' => $now]);
+
     }
 }
 
